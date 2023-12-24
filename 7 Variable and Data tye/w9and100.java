c18aff26e1672228481db63e19bd22962e56307b
Karansinh Rathod <115916190+K2R-krn@users.noreply.github.com>
2023-12-24 05:48:44 -0800

    Add files via upload

---
+++ 7 Variable and Data tye/w9and100.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class w9and100 {
+    public static void main(String args[]) {
+        int i = 10;
+        System.out.println(i);
+        while (i <= 99){
+            i = i + 2;
+            System.out.println(i);
+        }
+    }
+}
