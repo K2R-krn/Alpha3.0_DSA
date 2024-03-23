d3cc93e9f23be1507f314875830c69e0e7ccadee
Karansinh Rathod <krnsr.rthd@gmail.com>
2024-03-23 10:11:01 +0530

    '

--- 0Extra/DOUUBBTTfacto.java
+++ 0Extra/DOUUBBTTfacto.java
@@ -1,14 +1,12 @@
 import java.util.*;
-public class DOUUBBTTfacto {
-    public static int factorial(int n){
-        if (n == 0){
-            return 1;
-            }
-            int smallOutput = factorial(n - 1);
-            int output = n * smallOutput;
-            return output;
-        }
-    public static void main(String args[]){
-        System.out.println(factorial(5));
+@SuppressWarnings("unused")
+public class DOUUBBTTfacto{
+
+    public static void aa(int a){
+        
+    }
+    
+    public static void main(String[] args){
+
     }
 }
