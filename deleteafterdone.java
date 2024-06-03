3edf46892c1fe0a828afee4e4df6f7d2199d7cab
K2Rkrn <krnsr.rthd@gmail.com>
2024-06-03 23:07:03 +0300

    a

--- deleteafterdone.java
+++ deleteafterdone.java
@@ -282,11 +282,203 @@ public class deleteafterdone {
     //^     2 D   A R R A Y S
 
     
+    public static void SpiralMatrix(int arr[][]){
+
+        int n = arr.length;
+        int m = arr[0].length;
+        int sr=0 , er=n-1, sc=0, ec = m-1;
+        
+        while(sr<=er && sc<=ec){
+            // TOP
+            for(int j = sc;j<=ec;j++){
+                System.out.print(arr[sr][j]+"  ");
+            }
+
+            //RIGHT
+            for(int i = sr+1;i<=er;i++){
+                System.out.print(arr[i][ec]+"  ");
+            }
+
+            //BOTTOM
+            for(int j = ec-1;j>=sc;j--){
+                System.out.print(arr[er][j]+"  ");
+            }
+
+            //LEFT
+            for(int i =er-1;i>sr;i--){
+                System.out.print(arr[i][sc]+"  ");
+            }
+            sr++;
+            ec--;
+            sc++;
+            er--;
+        }
+        
+    }
+ 
+    public static void Prt2DArr(int arr[][]){
+        for(int i = 0;i<arr.length;i++){
+            for(int j= 0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static int DiagnolSum(int arr[][]){
+
+        int ts = 0;
+        
+        for(int i = 0;i<arr.length;i++){
+            //sum of Primary Diagnol
+            ts+=arr[i][i];
+            //sum of Secondary Diagnol
+            if(i!=arr.length-1-i){
+                ts+=arr[i][arr.length-1-i];
+            }
+        }
+        return ts;
+
+    }
+
+
+
+    public static boolean SearchinSortedMatrix(int arr[][], int key){
+        int row=0, col = arr[0].length-1;
+
+        while(row<arr.length && col>=0){
+            if(arr[row][col]==key){
+                System.out.println("Key found at : "+row+" , "+col); 
+                return true;
+            }
+            else if(arr[row][col]>key){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        System.out.println("Key not found ! ");
+        return false;
+
+    }
+    // public static void main(String args[]){
+        // int matrix[][] = new int[4][4];
+        // Scanner sc = new Scanner(System.in );
+        // int n = 4, m=4;
+        // for(int i = 0;i<n;i++){
+        //     for(int j= 0;j<m;j++){
+        //         matrix[i][j] = sc.nextInt();
+        //     }
+        // }
+    //     int matr[][] = {{1,2,3,4},
+    //                     {5,6,7,8},
+    //                     {9,10,11,12}
+    //                     ,{13,14,15,16}};
+    
+    //     // SpiralMatrix(matr);
+    //     // System.out.println(DiagnolSum(matr));
+    //     SearchinSortedMatrix(matr, 10);
+    // }
+
+
+
+    //^   S T R I N G S
+
+    public static boolean Pallindrome(String str){
+        for(int i = 0;i<str.length()/2;i++){
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void shortestPath(String str){
+
+        int x = 0, y=0;
+        for(int i = 0;i<str.length();i++){
+            char dir = str.charAt(i);
+            if(dir== 'N'){
+                y++;
+            }else if(dir=='S'){
+                y--;
+            }else if(dir=='W'){
+                x--;
+            }else if(dir=='E'){
+                x++;
+            }else{
+                System.out.println("INVALID INPUT");
+            }
+        }
+        int x2 = x*x;
+        int y2 = y*y;
+        float Shorp = (float)Math.sqrt(x2+y2);
+        // float ShorP = (float)Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
+        System.out.println("Shortest path = "+Shorp);
+    }
+
+
+    public static String toUpper(String str){
+
+        StringBuilder sb = new StringBuilder(str.length());
+
+     // Appends first word as no space before it so we do it manually and after this all the words will have space before them so loop forward
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i = 1;i<str.length();i++){
+            if(str.charAt(i)==' ' && i<str.length()-1){ // If empty spaces and more characters forward then 
+                sb.append(str.charAt(i));              // Copy empty space and 
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));// Copy other letters after converting them to uppercase
+            }else{
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    }
+
+    
+    public static String compress(String str){
+
+        StringBuilder sb = new StringBuilder(str.length());
+
+        for(int i = 0;i<str.length();i++){
+            int count = 1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1) ){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count>1){
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
 
     public static void main(String args[]){
+        // String str = "racecar";
+        // System.out.println(Pallindrome(str));
 
+        // String dir = "WNEENESENNN";
+        // shortestPath(dir);
+    
+        // String fruits[] = {"apple", "mango", "banana","pineapple"};
+        // String largestStr = fruits[0];
+        // for(int i=1;i<fruits.length;i++){
+        //     if(fruits[i].compareTo(largestStr)>0){
+        //         largestStr = fruits[i];
+        //     }
+        // }
+        // System.out.println(largestStr);
 
+        // String str = " hello WORLSD";
+        // System.out.println(toUpper(str));
 
-
+        String str = "aaabbcccdde";
+        System.out.println(compress(str));
     }
 } 
