86f9f691b3f0cb69855af33d1a94a1db31b1ab52
Karansinh Rathod <krnsr.rthd@gmail.com>
2024-09-10 19:48:51 +0530

    Todays questions

--- 43 44 45 Dynamic Programming/dpdp.java
+++ 43 44 45 Dynamic Programming/dpdp.java
@@ -515,6 +515,139 @@ public class dpdp {
         return ans;
     }
 
+
+
+
+    //* 14    Matrix Chain Multiplication      
+    
+    // TABULATION
+    public static int mcmTabu(int arr[]){
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+
+        // Initialization       // Also no need ot write this as default is 0 // But for logic we write it
+        for(int i = 0;i<n;i++){
+            dp[i][i] = 0;
+        }
+
+        // BOTTOM UP
+        for(int len = 2;len<=n-1;len++){
+            for(int i = 1;i<=n-len;i++){
+                int j = i+len-1; // COL
+                
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k =i;k<=j-1;k++){
+                    int c1 = dp[i][k];
+                    int c2 = dp[k+1][j];
+                    int c3 = arr[i-1]*arr[k]*arr[j];
+
+                    dp[i][j] = Math.min(dp[i][j], c1+c2+c3);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
+
+
+    // MEMO    O ( n^2 )
+    public static int mcmMemo(int arr[], int i, int j, int dp[][]){
+        if(i==j) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int ans = Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++){
+            int cost1 = mcmMemo(arr, i, k, dp);
+            int cost2 = mcmMemo(arr, k+1, j, dp);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+
+            ans = Math.min(ans, cost1 + cost2 + cost3);
+
+        }
+        return dp[i][j] = ans;
+
+    }
+
+
+
+    // BRUTE FORCE
+    public static int mcm(int arr[], int i, int j){
+        if(i==j){
+            return 0;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++){
+            int cost1 = mcm(arr, i, k); // Result size = Ai...Ak  = arr[i-1] x arr[k]
+            int cost2 = mcm(arr, k+1, j);// Result Size = Ai+1...Aj  = arr[k] x arr[j]
+            
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+
+            int finalCost = cost1+cost2+cost3;
+            ans = Math.min(ans, finalCost);
+        }
+        return ans; 
+    }
+
+
+    //* 14    Minimum Partitioning /  Minimum Subset Sum Difference  /  Partitioning Subset   O ( n * w )
+    public static int minPart(int arr[]){
+        int n = arr.length;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+
+        int W = sum/2;
+
+        int dp[][] = new int[n+1][W+1];
+
+
+        // Bottom up
+        for(int i=1;i<n+1;i++){
+            for(int j =1;j<W+1;j++){
+                // Valid condn
+                if(arr[i-1]<= j){
+                    // choice
+                    dp[i][j] = Math.max(arr[i-1] + dp[i-1][j-arr[i-1]] , dp[i-1][j]);
+                }else{ // invalid case
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        int sum1 = dp[ n][W];
+        int sum2 = sum - sum1;
+        return Math.abs(sum1-sum2);
+    }      
+
+
+    //* 15    Minimum Array Jumps   O ( n * w )
+    public static int minJumps(int arr[]){
+        int n = arr.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+
+        // initialize
+        dp[n-1] = 0;
+
+        //
+        for(int i=n-2;i>=0;i--){
+            int steps = arr[i];
+            int ans = Integer.MAX_VALUE;
+            for(int j=i+1;j<=i+steps && j<n;j++){
+                if(dp[j] != -1){
+                    ans = Math.min(ans, dp[j]+1);
+                }
+            }
+            if(ans != Integer.MAX_VALUE){
+                dp[i] = ans;
+            }
+        }
+        return dp[0];
+    }
+
+
+    
     public static void main(String args[]){
         // int n = 5; 
         // int f[] = new int[n+1];
@@ -603,14 +736,34 @@ public class dpdp {
         // System.out.println(wildCard(str1, str2));
 
         //^ 13
-        int dp[] = new int[6];
-        for(int i=0;i<dp.length;i++){
-            dp[i] = -1;
-        }
-        System.out.println(catlan(5));  
-        System.out.println(catlanMemo(5, dp));  
-        System.out.println(catlanTabu(5));  
+        // int dp[] = new int[6];
+        // for(int i=0;i<dp.length;i++){
+        //     dp[i] = -1;
+        // }
+        // System.out.println(catlan(5));  
+        // System.out.println(catlanMemo(5, dp));  
+        // System.out.println(catlanTabu(5));  
     
+        //^ 14
+        // int arr[] = {1,2,3,4,3};
+        // int n = arr.length;
+        // System.out.println(mcm(arr, 1, n-1));
+
+        // int dp[][] = new int[n][n];
+        // for(int i=0;i<n;i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // System.out.println(mcmMemo(arr, 1, n-1, dp));
+
+        // System.out.println(mcmTabu(arr));  
+
+        //^ 15
+        // int nums[] = {1, 6, 11, 5};
+        // System.out.println(minPart(nums));
+
+        //^ 16
+        int arr[] = {2,3,1,1,4};
+        System.out.println(minJumps(arr));
     }
 }
   
